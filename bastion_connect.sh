4742623e7fb6911c6d9df0c59ae6addc89613932
AmeerBadir <ameer.badir123@gmail.com>
2024-05-25 16:59:43 +0300

    firsr two cases

--- bastion_connect.sh
+++ bastion_connect.sh
@@ -8,3 +8,25 @@ if [ -z "$KEY_PATH" ]; then
     exit 5
 fi
 
+# if t
+if [ $# -lt 1 ]; then
+    echo "Failed: missing public  ip"
+    exit 5
+fi
+
+# creates variables
+PUBLIC_IP=$1
+PRIVATE_IP=$2
+
+# if there is only one argument then connect to public instance
+if [ $# -eq 1 ]; then
+    ssh -i "$KEY_PATH" ubuntu@"$PUBLIC_IP"
+
+# if the number of arguments is two, then the first one is the public instance ip and the
+# second one is private instance ip
+
+elif [ $# -eq 2 ]; then
+    #First connect to the public instance and then connect to private instance from the public.
+    ssh -i "$KEY_PATH" -t ubuntu@"$PUBLIC_IP" "ssh -i ameer-new-key-pair1.pem ubuntu@$PRIVATE_IP"
+
+fi
