d440dab436463ea08d1a04d94e7dc56cf6193052
AmeerBadir <ameer.badir123@gmail.com>
2024-05-25 15:16:44 +0300

    check if keyPath is exists

--- bastion_connect.sh
+++ bastion_connect.sh
@@ -1,1 +1,10 @@
 #!/bin/bash
+
+
+
+# Check if KEY_PATH environment variable exists or not set
+if [ -z "$KEY_PATH" ]; then
+    echo "ERROR: Environment variable KEY_PATH is not set."
+    exit 5
+fi
+
