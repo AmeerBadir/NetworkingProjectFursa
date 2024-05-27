0e1965f6c7f81ee45044c575b51ea203892d818a
AmeerBadir <100073193+AmeerBadir@users.noreply.github.com>
2024-05-27 22:08:55 +0300

    Update tlsHandshake.sh

--- tlsHandshake.sh
+++ tlsHandshake.sh
@@ -1,41 +1,67 @@
 #!/bin/bash
 
 #Check if instance ip exist
-if [ $# -ne 1 ]; then
-    echo "Error: Missing public instance ip"
-    exit 1
-fi
+# if [ $# -ne 1 ]; then
+#     echo "Error: Missing public instance ip"
+#     exit 1
+# fi
 
-SERVER_IP=$1
-SESSION_ID=""
-MASTER_KEY=""
-SAMPLE_MESSAGE="Hi server, please encrypt me and send to client!"
+# SERVER_IP=$1
+# SESSION_ID=""
+# MASTER_KEY=""
+# SAMPLE_MESSAGE="Hi server, please encrypt me and send to client!"
 
 
+# # Step 1: Client Hello
+# echo "Sending Client Hello..."
+# CLIENT_HELLO=$(curl -s -X POST -H "Content-Type: application/json" -d '{"version": "1.3", "ciphersSuites": ["TLS_AES_128_GCM_SHA256", "TLS_CHACHA20_POLY1305_SHA256"], "message": "Client Hello"}' http://$SERVER_IP:8080/clienthello)
+# if [ $? -ne 0 ]; then
+#     echo "Error sending Client Hello"
+#     exit 2
+# fi
+
+# CIPHER_SUITE=$(echo "$CLIENT_HELLO" | jq -r '.')
+# SESSION_ID=$(echo "$CIPHER_SUITE" | jq -r '.sessionID')
+# SERVER_CERT=$(echo "$CIPHER_SUITE" | jq -r '.serverCert')
+
+# # Step 2: Server Certificate Verification
+# echo "Verifying server certificate..."
+# echo "$SERVER_CERT"  > cert.pem
+# # wget -q -O cert_ca_aws.pem https://alonitac.github.io/DevOpsTheHardWay/networking_project/cert-ca-aws.pem
+# wget https://alonitac.github.io/DevOpsTheHardWay/networking_project/cert-ca-aws.pem
+# if [ ! -f cert_ca_aws.pem ]; then
+#     echo "Failed to download CA certificate."
+#     exit 2
+# fi
+# openssl verify -CAfile cert-ca-aws.pem cert.pem
+
+# ##############
+# rm cert-ca-aws.pem
 # Step 1: Client Hello
-echo "Sending Client Hello..."
+echo "Step 1: Sending Client Hello..."
 CLIENT_HELLO=$(curl -s -X POST -H "Content-Type: application/json" -d '{"version": "1.3", "ciphersSuites": ["TLS_AES_128_GCM_SHA256", "TLS_CHACHA20_POLY1305_SHA256"], "message": "Client Hello"}' http://$SERVER_IP:8080/clienthello)
 if [ $? -ne 0 ]; then
-    echo "Error sending Client Hello"
+    echo "Failed to send Client Hello"
     exit 2
 fi
 
-CIPHER_SUITE=$(echo "$CLIENT_HELLO" | jq -r '.')
-SESSION_ID=$(echo "$CIPHER_SUITE" | jq -r '.sessionID')
-SERVER_CERT=$(echo "$CIPHER_SUITE" | jq -r '.serverCert')
+# Step 2: Server Hello
+echo "Step 2: Received Server Hello..."
+SERVER_HELLO=$(echo "$CLIENT_HELLO" | jq -r '.')
+SESSION_ID=$(echo "$SERVER_HELLO" | jq -r '.sessionID')
+SERVER_CERT=$(echo "$SERVER_HELLO" | jq -r '.serverCert')
+echo "$SERVER_CERT" > cert.pem
 
-# Step 2: Server Certificate Verification
-echo "Verifying server certificate..."
-echo "$SERVER_CERT"  > cert.pem
-# wget -q -O cert_ca_aws.pem https://alonitac.github.io/DevOpsTheHardWay/networking_project/cert-ca-aws.pem
+
+# Step 3: Server Certificate Verification
+echo "Step 3: Verifying Server Certificate..."
 wget https://alonitac.github.io/DevOpsTheHardWay/networking_project/cert-ca-aws.pem
-if [ ! -f cert_ca_aws.pem ]; then
-    echo "Failed to download CA certificate."
-    exit 2
-fi
 openssl verify -CAfile cert-ca-aws.pem cert.pem
+if [ $? -ne 0 ]; then
+    echo "Server Certificate is invalid."
+    exit 5
+fi
 
-##############
 rm cert-ca-aws.pem
 
 
