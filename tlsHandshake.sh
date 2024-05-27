aec87389c878b5644e286dc6eca42ded75951987
AmeerBadir <100073193+AmeerBadir@users.noreply.github.com>
2024-05-27 21:56:00 +0300

    Update tlsHandshake.sh

--- tlsHandshake.sh
+++ tlsHandshake.sh
@@ -21,13 +21,14 @@ if [ $? -ne 0 ]; then
 fi
 
 CIPHER_SUITE=$(echo "$CLIENT_HELLO" | jq -r '.')
-SESSION_ID=$(echo "$CLIENT_HELLO" | jq -r '.sessionID')
-SERVER_CERT=$(echo "$CLIENT_HELLO" | jq -r '.serverCert')
+SESSION_ID=$(echo "$CIPHER_SUITE" | jq -r '.sessionID')
+SERVER_CERT=$(echo "$CIPHER_SUITE" | jq -r '.serverCert')
 
 # Step 2: Server Certificate Verification
 echo "Verifying server certificate..."
 echo "$SERVER_CERT"  > cert.pem
-wget -q -O cert_ca_aws.pem https://alonitac.github.io/DevOpsTheHardWay/networking_project/cert-ca-aws.pem
+# wget -q -O cert_ca_aws.pem https://alonitac.github.io/DevOpsTheHardWay/networking_project/cert-ca-aws.pem
+wget https://alonitac.github.io/DevOpsTheHardWay/networking_project/cert-ca-aws.pem
 if [ ! -f cert_ca_aws.pem ]; then
     echo "Failed to download CA certificate."
     exit 2
@@ -37,25 +38,25 @@ if [ $? -ne 0 ]; then
     echo "Server Certificate is invalid."
     exit 5
 fi
-#rm cert-ca-aws.pem
+rm cert-ca-aws.pem
 echo "Generating and encrypting master key..."
 MASTER_KEY=$(openssl rand -base64 32)
-ENCRYPTED_MASTER_KEY=$(echo $MASTER_KEY | openssl smime -encrypt -aes-256-cbc -binary -outform DER cert.pem | base64 -w 0)
-#ENCRYPTED_MASTER_KEY=$(openssl smime -encrypt -aes-256-cbc -in <(echo "$MASTER_KEY") -outform DER cert.pem | base64 -w 0)
+#ENCRYPTED_MASTER_KEY=$(echo $MASTER_KEY | openssl smime -encrypt -aes-256-cbc -binary -outform DER cert.pem | base64 -w 0)
+ENCRYPTED_MASTER_KEY=$(openssl smime -encrypt -aes-256-cbc -in <(echo "$MASTER_KEY") -outform DER cert.pem | base64 -w 0)
 # Step 4: Send encrypted master key to server
 echo "Sending encrypted master key to server..."
-KEY_EXCHANGE=$(curl -s -X POST -H "Content-Type: application/json" -d "{\"sessionID\": \"$SESSION_ID\", \"masterKey\": \"$ENCRYPTED_MASTER_KEY\", \"sampleMessage\":  \"$SAMPLE_MESSAGE\"}" http://${SERVER_IP}:8080/keyexchange)
+KEY_EXCHANGE=$(curl -s -X POST -H "Content-Type: application/json" -d "{\"sessionID\": \"$SESSION_ID\", \"masterKey\": \"$ENCRYPTED_MASTER_KEY\", \"sampleMessage\":  \"$SAMPLE_MESSAGE\"}" http://$SERVER_IP:8080/keyexchange)
 if [ $? -ne 0 ]; then
     echo "Error sending encrypted master key"
     exit 4
 fi
 
 # Parse server response
-ENCRYPTED_SAMPLE_MESSAGE=$(echo $KEY_EXCHANGE | jq -r '.encryptedSampleMessage')
+ENCRYPTED_SAMPLE_MESSAGE=$(echo "$KEY_EXCHANGE" | jq -r '.encryptedSampleMessage')
 
 # Step 5: Decrypt sample message and verify
 echo "Decrypting and verifying sample message..."
-DECRYPTED_SAMPLE_MESSAGE=$(echo $ENCRYPTED_SAMPLE_MESSAGE | base64 -d | openssl enc -d -aes-256-cbc -pbkdf2 -k $MASTER_KEY)
+DECRYPTED_SAMPLE_MESSAGE=$(echo $ENCRYPTED_SAMPLE_MESSAGE | base64 -d | openssl enc -d -aes-256-cbc -pbkdf2 -k "$MASTER_KEY")
 if [ "$DECRYPTED_SAMPLE_MESSAGE" != "$SAMPLE_MESSAGE" ]; then
     echo "Server symmetric encryption using the exchanged master-key has failed."
     exit 6
