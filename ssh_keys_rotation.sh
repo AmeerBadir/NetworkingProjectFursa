100208cb95b1ce23839e9d33a60a9a3636dc509f
AmeerBadir <100073193+AmeerBadir@users.noreply.github.com>
2024-05-27 23:11:18 +0300

    Update ssh_keys_rotation.sh

--- ssh_keys_rotation.sh
+++ ssh_keys_rotation.sh
@@ -1,38 +1,12 @@
 #!/bin/bash
 
-
-
 if [ $# -ne 1 ]; then
-
   echo "Missing IP address"
-
   exit 1
-
 fi
 
-
-
-# generate key check
-
-# ssh-keygen -t rsa -b 2048 -f check-key.pem -N ""
-
-
-
 # an order to do rotation, we need to genrate a new key and then add it in authorized key
-
-# in private instance
-
-
-
-
-
-# create a file to copy to the current key to it
-
-#touch copy-key.pem copy-key.pem.pub
-
-
-
-# if the key exists, then copy it 
+# in private instance 
 
 if [ -e ameer-new-key-pair1.pem ]; then
 
@@ -48,12 +22,8 @@ if [ -e ameer-new-key-pair1.pem ]; then
 
 fi
 
-#chmod 400 copy-key.pem
-
 # create a key and override the pervious one
 
 ssh-keygen -t rsa -b 4096 -f ameer-new-key-pair1.pem -N ""
-
-
 
 cat ameer-new-key-pair1.pem.pub | ssh -o StrictHostKeyChecking=accept-new -i copy-key.pem ubuntu@"$1" "cat > ~/.ssh/authorized_keys"
